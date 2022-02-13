7721aa95933c461bfd9bdc6f51161c6e7b305c52
Kamila <kadakanova@gmail.com>
2022-02-12 21:50:53 -0500

    Solution of Task LowerCaseLetters gives SyntaxError: Unexpected string in CodeWars

---
+++ kata8/lowercaseletters.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/58aacf3e048560c705000799/train/javascript
+//Convert a string to lowercase letters.
+//Example: convert "Hello world!" to "hello world!"
+
+// function convertLowerCase(str) {
+//     //...
+//     }
+
+    let convertLowerCase = "Hello World!"
+    let result =  convertLowerCase.toLowerCase()
+    console.log(result)
