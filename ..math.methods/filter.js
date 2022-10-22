567f8a9381a3ee229627254586a33a4b204a120f
Kamila Adakanova <kadakanova@gmail.comgit config --global user.name Kamila>
2022-10-21 23:26:21 -0400

    ..string.methods and substring

---
+++ ..math.methods/filter.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/580435ab150cca22650001fb/solutions/javascript
+//Array.prototype.filter()
+
+//The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.
+
+const filterLucky = (xs) => xs.filter((x) => x.toString().includes('7')); //
---
+++ kata7/mispelledWord.js
@@ -0,0 +1,54 @@
+//https://www.codewars.com/kata/5892595f190ca40ad0000095/train/javascript
+
+const str1 = 'versed'; //
+const str2 = 'v5rsed'; //
+
+var mispelled = function (word1, word2) {
+  if (
+    word1 === word2.substring(0, word2.length - 1) ||
+    word2 === word1.substring(1, word1.length) ||
+    word1 === word2.substring(1, word2.length) ||
+    word2 === word1.substring(0, word2.length - 1)
+  ) {
+    return true;
+  }
+
+  let count = 0;
+  for (let i = 0; i < word1.length; i++) {
+    if (word1[i] !== word2[i]) {
+      count++;
+    }
+  }
+  return count <= 1 ? true : false;
+};
+
+//
+// function mispelled(word1, word2) {
+//   if (word1.length > word2.length) {
+//     return compareNotEqual(word1, word2);
+//   }
+//   if (word1.length < word2.length) {
+//     return compareNotEqual(word2, word1);
+//   }
+//   {
+//     return compareEqualStr(word1, word2);
+//   }
+// }
+// function compareNotEqual(longW, shortW) {
+//   return (
+//     shortW === longW.substring(0, longW.length - 1) ||
+//     shortW === longW.substring(1, longW.length)
+//   );
+// }
+// function compareEqualStr(word1, word2) {
+//   let count = 0;
+//   for (let i = 0; i < word1.length; i++) {
+//     if (word1[i] != word2[i]) {
+//       count++;
+//     }
+//   }
+//   return count > 1 ? false : true;
+// }
+
+res = mispelled(str1, str2);
+console.log(res);
